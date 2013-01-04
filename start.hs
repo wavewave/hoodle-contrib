7001bf015c3eeb3a159fac4503a809dbfda1a303
Ian-Woo Kim <ianwookim@gmail.com>
2013-01-03 22:53:57 -0500

    init my hoodle.d script as a git repo

---
+++ start.hs
@@ -0,0 +1,129 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Main where 
+
+import           Control.Category
+import           Control.Lens hiding ((<.>))
+import qualified Data.ByteString.Char8 as B 
+import           Data.Monoid 
+import           Data.Time.Clock 
+import           Graphics.Rendering.Cairo 
+import           Graphics.UI.Gtk hiding (get,set)
+import           System.FilePath
+-- 
+import           Data.Hoodle.BBox
+import           Data.Hoodle.Simple
+import           Graphics.Hoodle.Render
+import           Graphics.Hoodle.Render.Item 
+import           Hoodle.Script
+import           Hoodle.Script.Hook
+import           Hoodle.StartUp
+-- 
+import Prelude hiding ((.),id)
+
+-- |
+makesvg :: [Item] -> FilePath -> IO () 
+makesvg itms fp = do 
+  mr <- renderitems itms 
+  case mr of 
+    Nothing -> return () 
+    Just (BBox (ulx,uly) (lrx,lry),r) -> 
+      withSVGSurface fp (lrx-ulx) (lry-uly) $ \s -> renderWith s r
+
+-- |
+makepng :: [Item] -> FilePath -> IO () 
+makepng itms fp = do 
+  mr <- renderitems itms 
+  case mr of 
+    Nothing -> return () 
+    Just (BBox (ulx,uly) (lrx,lry),r) -> 
+      withImageSurface FormatARGB32 (floor (lrx-ulx)) (floor (lry-uly)) $ 
+        \s -> do 
+          renderWith s r 
+          surfaceWriteToPNG s fp 
+
+-- |
+renderitems :: [Item] -> IO (Maybe (BBox, Render ()))
+renderitems itms = do
+  ritms <- mapM cnstrctRItem itms 
+  let ulbbox = unUnion . mconcat . fmap (Union . Middle . getBBox) $ ritms
+  case ulbbox of 
+    Middle (bbox@(BBox (ulx,uly) (lrx,lry))) -> 
+      let r = do translate (-ulx) (-uly)  
+                 mapM_ renderRItem ritms  
+      in return (Just (bbox,r))
+    _ -> return Nothing 
+
+-- |
+main :: IO ()
+main = hoodleStartMain defaultScriptConfig 
+         { message = Just welcomeMessage 
+         , hook = Just newhook } 
+       -- { message = welcomeMessage } 
+
+-- |
+custommenuhook :: [Item] -> IO ()
+custommenuhook is = do 
+  ctime <- getCurrentTime
+  -- makesvg is ("/home/wavewave/Dropbox/memos" </> show ctime <.> "svg") 
+  makepng is ("/home/wavewave/Dropbox/memos" </> show ctime <.> "png") 
+
+-- |
+fnamesuggest :: IO FilePath 
+fnamesuggest = do 
+  ctime <- getCurrentTime
+  return (show ctime <.> "hdl")
+
+-- | 
+newhook :: Hook 
+newhook = defaultHook { saveAsHook = Nothing -- sahk 
+                      , afterSaveHook = Nothing -- Just ashk
+                      , afterUpdateClipboardHook = Just auch 
+                      , customContextMenuTitle = Just "take a memo"
+                      , customContextMenuHook = Just custommenuhook 
+                      , fileNameSuggestionHook = Just fnamesuggest
+                      } 
+          
+-- | after update clipboard hook
+auch :: [Item] -> IO ()
+auch itms = putStrLn ("length of strokes = " ++ show (length itms))
+
+
+-- | 
+sahk :: Hoodle -> IO () 
+sahk hdl = do 
+  let ttl = view title hdl
+  putStrLn $ show ttl 
+  putStrLn "Save As Hook called"
+
+-- |
+ashk :: Hoodle -> IO ()
+ashk hdl = do 
+  putStrLn "AfterSaveHook called"
+  dialog <- messageDialogNew Nothing [DialogModal] 
+              MessageQuestion ButtonsOkCancel 
+              "Do you want to upload your file now?"
+  res <- dialogRun dialog 
+  case res of
+    ResponseOk -> do 
+      widgetDestroy dialog
+      putStrLn "you pushed ok."      
+      B.putStrLn (view title hdl)
+      return ()
+    _ -> do 
+      widgetDestroy dialog
+      return ()
+  
+  
+-- |
+welcomeMessage :: String 
+welcomeMessage = 
+   " ===================================================\n\
+   \ =                                                 =\n\ 
+   \ =        welcome to hoodle                        =\n\
+   \ =                                                 =\n\
+   \ =           Copyright 2011-2013  Ian-Woo Kim      =\n\
+   \ =                                                 =\n\
+   \ =                            Date: 2013.01.03     =\n\
+   \ =                                                 =\n\
+   \ ===================================================\n"
